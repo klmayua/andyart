fac2c088254b9c05edf224b3663dabad506d5ad0
JorelOdero <dedan.odero@strathmore.edu>
2026-04-22 00:48:49 +0300

    Add ignoreBuildErrors to next.config.js

--- next.config.js
+++ next.config.js
@@ -9,6 +9,14 @@ const nextConfig = {
       },
     ],
   },
+  // Ignore TypeScript errors during build
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // Ignore ESLint errors during build  
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
